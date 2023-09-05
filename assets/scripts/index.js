bc9a23f7763bc0e6c9133278c6e5848ffac23c3b
Adri Antori <adriantori11@gmail.com>
2023-09-05 07:29:20 +0700

    feat: mobile navbar done

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,34 @@
-$(document).ready(function(){
-    $('#displayCartData').mouseenter(function () { 
-        $('#cartData').css('display', 'block');
+$(document).ready(function () {
+    $('#displayCartData').mouseenter(function () {
+        $('#cartData').show();
     });
-    $('#displayCartData').mouseleave(function () { 
-        $('#cartData').css('display', 'none');
+    $('#displayCartData').mouseleave(function () {
+        $('#cartData').hide();
+    });
+    $('#cartData').mouseenter(function () {
+        $('#cartData').show();
+    });
+    $('#cartData').mouseleave(function () {
+        $('#cartData').hide();
+    });
+
+    $(".cross").hide();
+    $(".menu").hide();
+    $(".hamburger").click(function () {
+        $(".menu").animate({
+            height: "toggle",
+            opacity: "toggle"
+        }, "slow");
+        $(".hamburger").hide();
+        $(".cross").show();
+    });
+
+    $(".cross").click(function () {
+        $(".menu").animate({
+            height: "toggle",
+            opacity: "toggle"
+        }, "slow");
+        $(".cross").hide();
+        $(".hamburger").show();
     });
 });
